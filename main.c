7bb856222aacb5e44915ee2ac5c270d7aed79031
JiarouDeng <dpj5243@psu.edu>
2020-09-17 20:52:16 +0000

    add space

--- main.c
+++ main.c
@@ -13,11 +13,11 @@ int sum_n(int n);
 // You may assume n >= 0, print s n times, each time in its own line
 void print_n(const char *s, int n);
 int main(void){
- char*n = readline("Enter an int:");
+ char*n = readline("Enter an int: ");
  int c = atof(n);
  double sum1 = sum_n(c);
  printf("sum is %f\n",sum1);
- char*f = readline("Enter a string:");
+ char*f = readline("Enter a string: ");
  print_n(f,c);
 }
 int sum_n(int n){
