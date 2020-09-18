6f3c430aa09d5ebe0a180273729fc8f037eefc96
JiarouDeng <dpj5243@psu.edu>
2020-09-18 00:58:56 +0000

    change to function

--- main.c
+++ main.c
@@ -13,22 +13,26 @@ int sum_n(int n);
 // You may assume n >= 0, print s n times, each time in its own line
 void print_n(const char *s, int n);
 int main(void){
- char*n = readline("Enter an int: ");
- int c = atof(n);
- double sum1 = sum_n(c);
- printf("sum is %f\n",sum1);
- char*f = readline("Enter a string: ");
- print_n(f,c);
+  char*n = readline("Enter an int: ");
+  int c = atof(n);
+  double sum1 = sum_n(c);
+  printf("sum is %f\n",sum1);
+  char*f = readline("Enter a string: ");
+  print_n(f,c);
 }
 int sum_n(int n){
-  int a, sum=0;
-  for(a=0; a<=(n); a++){
-   sum+=a;
-}
-return sum;
+  if (n > 0){
+    return n+sum_n(int n);
+    }
+  else{
+    return n;
+  }
+  
 }
 void print_n(const char *s, int n){
-  int b;
-  for(b=0; b<=n; b++)
-   printf("%s",s);
+  if (n>0){
+    printf("%s",s);
+    print_n(s,n-1);
+  }
+  
 }
